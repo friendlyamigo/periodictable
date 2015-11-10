562e2442b70972791891cef3c60c45f42e367d30
jackfriend <jackwillamfriend@gmail.com>
2015-11-09 20:00:40 -0500

    added element charges

--- functions.js
+++ functions.js
@@ -1,9 +1,10 @@
 $.ajax({
-  url: 'elements.json',
+  url: 'elements.json', // get element json
   success: function(result){
-    for (var i = 0; i < result.element.length; i++){
+    for (var i = 0; i < result.element.length; i++){ // for element in element json
       element = result.element[i];
-      $('.' + element.symbol.toLowerCase()).append(element.symbol);
-    }
+      $('.' + element.symbol.toLowerCase()).append(element.symbol); // put element symbol in its div
+      console.log(element.name, element.charge); // console.log element and its charge(s)
+    };
   }
 });
