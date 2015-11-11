bfba2147a197bd4a5d10ecc62070f835f7371811
jackfriend <jackwillamfriend@gmail.com>
2015-11-10 19:42:42 -0500

    started front end

--- functions.js
+++ functions.js
@@ -4,7 +4,7 @@ $.ajax({
     for (var i = 0; i < result.element.length; i++){ // for element in element json
       element = result.element[i];
       $('.' + element.symbol.toLowerCase()).append(element.symbol); // put element symbol in its div
-      console.log(element.name, element.charge); // console.log element and its charge(s)
+      // console.log(element.name, element.charge); // console.log element and its charge(s)
     };
   }
 });
